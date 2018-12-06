1eb8e158802d09d2dc72c52bb5f07dfd6f4a7783
Christie Lafrance <christie.lafrance@gmail.com>
2018-12-06 02:50:24 -0800

    add gauge

--- static/js/app.js
+++ static/js/app.js
@@ -23,8 +23,9 @@ function buildMetadata(sample) {
       };
     });
 
-  // BONUS: Build the Gauge Chart
-  // buildGauge(data.WFREQ);
+    // BONUS: Build the Gauge Chart
+    // buildGauge();
+    buildGauge(data.WFREQ);
   });
 }
 
@@ -85,12 +86,6 @@ function buildCharts(sample) {
     var otu_ids_sorted = [];
     var otu_labels_sorted = [];
 
-    // sample_values_sorted.sort(function compareFunction(firstNum, secondNum) {
-    //   return secondNum - firstNum;
-    // });
-
-    // sample_values_sorted = sample_values_sorted.slice(0, 10);
-
     sample_values_sorted = sample_values_sorted.sort(function compareFunction(firstNum, secondNum) {
       return secondNum - firstNum;
     }).slice(0, 10);
@@ -106,32 +101,21 @@ function buildCharts(sample) {
       otu_ids_sorted.push(otu_ids[index_of_sample_value]);
       otu_labels_sorted.push(otu_labels[index_of_sample_value]);
 
-      // console.log("index: " + index_of_sample_value);
-      // console.log("sample_value: " + sample_values_sorted[i]);
-      // console.log("otu_is: " + otu_id);
-      // console.log("sample Values: " + sample_values);
-      // console.log("length:" + otu_ids.length);
-      // console.log("otu ids: " + otu_ids);
-      // console.log("");
-
       sample_values.splice(index_of_sample_value, 1 );
       otu_ids.splice(index_of_sample_value, 1 );
       otu_labels.splice(index_of_sample_value, 1 );
     };
 
-    console.log(sample_values_sorted);
-    console.log(otu_ids_sorted);
-
     data = [{
       values: sample_values_sorted,
       labels: otu_ids_sorted,
-      text: otu_labels_sorted,
+      // text: otu_labels_sorted,
       type: "pie"
     }];
 
     var 
     layout = {
-      autosize: false,
+      // autosize: false,
       height: 450,
       width: 450,
       margin: {
@@ -173,47 +157,146 @@ function optionChanged(newSample) {
   buildMetadata(newSample);
 }
 
+
+function buildGauge(belly_data) {
+  // Enter a speed between 0 and 180
+  var level = belly_data;
+
+  // Trig to calc meter point
+  var degrees = 9 - level,
+       radius = .5;
+  var radians = degrees * Math.PI / 9;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+       pathX = String(x),
+       space = ' ',
+       pathY = String(y),
+       pathEnd = ' Z';
+  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+  var data = [{ type: 'scatter',
+     x: [0], y:[0],
+      marker: {size: 28, color:'850000'},
+      showlegend: false,
+      name: 'speed',
+      text: level,
+      hoverinfo: 'text+name'},
+    { values: [20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20/9, 20],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6',
+              '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {colors:['rgba(141, 179, 131, .5)', 'rgba(131, 165, 129, .5)', 'rgba(106, 134, 98, .5)',
+                     'rgba(181, 196, 145, .5)', 'rgba(200, 211, 150, .5)', 'rgba(216, 216, 170, .5)', 
+                     'rgba(229, 228, 199, .5)', 'rgba(243, 243, 229, .5)',  'rgba(247, 243, 236, .5)', 'rgba(255, 255, 255, 0)']},
+    labels: ['8.1-9', '7.1-8', '6.1-7', '5.1-6', '4.1-5', '3.1-4', '2.1-3', '1.1-2', '0-1', ''],
+    hoverinfo: 'label',
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+  }];
+
+  var layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+          color: '850000'
+        }
+      }],
+    title: 'Belly Button Washing Times per Week',
+    height: 500,
+    width: 500,
+    margin: {
+      l: 50,
+      r: 0,
+      b: 0,
+      t: 150,
+      pad: 4
+    },
+    xaxis: {zeroline:false, showticklabels:false,
+               showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+               showgrid: false, range: [-1, 1]}
+  };
+
+  Plotly.newPlot('gauge', data, layout);
+}
+
+
+// function buildGauge() {
+//   // Enter a speed between 0 and 180
+//   var level = 175;
+
+//   // Trig to calc meter point
+//   var degrees = 180 - level,
+//        radius = .5;
+//   var radians = degrees * Math.PI / 180;
+//   var x = radius * Math.cos(radians);
+//   var y = radius * Math.sin(radians);
+
+//   // Path: may have to change to create a better triangle
+//   var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+//        pathX = String(x),
+//        space = ' ',
+//        pathY = String(y),
+//        pathEnd = ' Z';
+//   var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+//   var data = [{ type: 'scatter',
+//      x: [0], y:[0],
+//       marker: {size: 28, color:'850000'},
+//       showlegend: false,
+//       name: 'speed',
+//       text: level,
+//       hoverinfo: 'text+name'},
+//     { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+//     rotation: 90,
+//     text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
+//               'Slow', 'Super Slow', ''],
+//     textinfo: 'text',
+//     textposition:'inside',
+//     marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+//                            'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+//                            'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+//                            'rgba(255, 255, 255, 0)']},
+//     labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+//     hoverinfo: 'label',
+//     hole: .5,
+//     type: 'pie',
+//     showlegend: false
+//   }];
+
+//   var layout = {
+//     shapes:[{
+//         type: 'path',
+//         path: path,
+//         fillcolor: '850000',
+//         line: {
+//           color: '850000'
+//         }
+//       }],
+//     title: 'Gauge Speed 0-100',
+//     height: 1000,
+//     width: 1000,
+//     xaxis: {zeroline:false, showticklabels:false,
+//                showgrid: false, range: [-1, 1]},
+//     yaxis: {zeroline:false, showticklabels:false,
+//                showgrid: false, range: [-1, 1]}
+//   };
+
+//   Plotly.newPlot('gauge', data, layout);
+// }
+
+
 // Initialize the dashboard
 init();
 
 
 
 
-
-
-
-
-function sortData (data) {
-    // User JSON parse() and stringify() to break references.
-    var sample_values = data.sample_values;
-
-    // By default array sort is as strings, so for numbers
-    // we must supply a compare function.
-    sample_values.sort(function (a, b) {
-        if (a > b) {
-            return -1;
-        }
-        if (a < b) {
-            return 1;
-        }
-        return 0;
-    });
-    
-    // We use this to gather names for the sorted ids.
-    var names = [];
-    // Iterate over the sorted ids.
-    ids.forEach(function (id, index) {
-        // Get the index of the id in the object to sort.
-        var objIdsIndex = objectToSort.ids.indexOf(id);
-
-        // Get the corresponding name for the id.
-        var nameAtIndex = objectToSort.names[objIdsIndex];
-
-        // Put the name at the same index as the sorted ids.
-        names.push(nameAtIndex);                
-    });
-    // Replace the ids and names of the object to sort with
-    // the sorted its and the names we put in the same order.
-    objectToSort.ids = ids;
-    objectToSort.names = names;
-}
